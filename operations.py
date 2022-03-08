9e15560bb1409905d27d18713abd62a5bab8a2de
NitishSAM <101179856+NitishSAM@users.noreply.github.com>
2022-03-08 16:00:52 +0530

    Update and rename README.md to operations.py

---
+++ operations.py
@@ -0,0 +1,12 @@
+# PythoneNS
+a=int(input("Enter a number1:"))
+b=int(input("Enter a number2:"))
+c=int(input("Enter a number3:"))
+sum=a+b+c
+product=a*b*c
+diff=a-b-c
+divison=a/b/c
+print("sum of given numbers is;",sum)
+print("product of given numbers is;",product)
+print("diff of given numbers is;",diff)
+print("division of given numders is;",divison)       
